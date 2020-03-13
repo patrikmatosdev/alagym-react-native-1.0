1418a5df4923983278bb0a438fb3456d94ccbf82
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-13 10:15:47 -0300

    Update text

--- Alagym/src/views/Help/index.js
+++ Alagym/src/views/Help/index.js
@@ -6,7 +6,7 @@ export default function Help(props) {
   // const mensagem = props.route.params.mensagem;
   //const { mensagem } = props.route.params;
   // const {params: { mensagem }} = props.route;
-  const mensagem = 'o gio e gay';
+  const mensagem = 'Teste';
 
   return (
     <View>
