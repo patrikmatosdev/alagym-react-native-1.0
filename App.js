64f9e3f4ec75f537bd6b5101c9f7527bf3746acc
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-17 17:03:43 -0300

    Renaming function

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 // import AppStack from './src/pages/routes/AppStack';
 import BottomStack from './src/routes/BottomStack';
 
-
 const App = () => {
+
   const logado = true;
 
   return (
