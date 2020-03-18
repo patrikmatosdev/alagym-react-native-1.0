c0ab696dfbd19d149737d7c9de075aad7e096f57
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-18 11:56:57 -0300

    Temporary removal

--- src/api/fetchExercicies.js
+++
@@ -1,6 +0,0 @@
-import Axios from 'axios';
-
-export default async function fetchExercicies() {
-  Alert('Produtos coletados com sucesso');
-  return await Axios.get('http://localhost:8080/exercicies')
-}
