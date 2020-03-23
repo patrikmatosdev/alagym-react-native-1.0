a193f957be2b1fd24f5cc622143ef3cf9d588751
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-23 12:19:10 -0300

    Create new component

--- src/components/MyWorkouts/TrainingDetail.js
+++ src/components/MyWorkouts/TrainingDetail.js
@@ -1,11 +1,19 @@
 import  React  from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-
 export default function TrainingDetail() {
   return(
-    <View>
-      <Text>Traing Detail</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>1</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{},
+  text: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
